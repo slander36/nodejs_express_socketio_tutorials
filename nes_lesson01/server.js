c501fb658d293d333102b9d65d43de272e16be6c
Sean Lander <s.lander36@gmail.com>
2011-11-03 15:02:44 -0500

    fixing lessone 1 to have less complexity

--- nes_lesson01/server.js
+++ nes_lesson01/server.js
@@ -3,7 +3,5 @@
  */
 
 var server = require('./server/main.js');
-var server2 = require('./server2/main.js');
 
 server.start();
-server2.start();
--- nes_lesson01/server/main.js
+++ nes_lesson01/server/main.js
@@ -4,11 +4,18 @@
 
 var app = require('express').createServer();
 var io = require('socket.io').listen(app);
+var fs = require('fs');
 
 function start() {
-	console.log('Socket server started');
+	console.log('Server started');
 	io.sockets.on('connection', function(socket) {
 		socket.emit('welcome',{message:'Welcome, user.'});
+	});
+	
+	app.get('*',function(req,res) {
+		fs.readFile('./www/index.html',function(err,buffer) {
+			res.write(buffer.toString());
+		});
 	});
 	
 	app.listen(1337);
