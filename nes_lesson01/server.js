e2925476d59ab82c24a6794d3ba20db4dafc0fdd
Sean Lander <s.lander36@gmail.com>
2011-11-03 15:21:27 -0500

    Updating lesson01 to be simpler and adding comments

--- nes_lesson01/server.js
+++ nes_lesson01/server.js
@@ -2,6 +2,8 @@
  * 
  */
 
+// Create a server based on server/main.js
 var server = require('./server/main.js');
 
+// Tell the server module to call its start function
 server.start();
--- nes_lesson01/server/main.js
+++ nes_lesson01/server/main.js
@@ -2,23 +2,40 @@
  * 
  */
 
+// Create an Express app
 var app = require('express').createServer();
+// Create a socket listener for the app
 var io = require('socket.io').listen(app);
+// Initialize a file system
 var fs = require('fs');
 
+// Module's start function
 function start() {
+	// send server console (terminal) a message
 	console.log('Server started');
+	// Initialize sockets. On being sent 'connection' from a client
+	// it will grab the client's information and send them back
+	// the 'welcome' message (see index.html for the other side
+	// of the equation
 	io.sockets.on('connection', function(socket) {
 		socket.emit('welcome',{message:'Welcome, user.'});
 	});
 	
+	// Tell the app that any request it gets from the user should
+	// respond the same
 	app.get('*',function(req,res) {
+		// The file system will read index.html and send the file's
+		// contents back as a string
 		fs.readFile('./www/index.html',function(err,buffer) {
-			res.write(buffer.toString());
+			res.send(buffer.toString());
 		});
 	});
 	
+	// Tell the app to use port 1337 to listen for any calls, whether
+	// it be web page requests or socket requests
 	app.listen(1337);
 };
 
+// Let other parts of Node.js know this is a module with a name
+// This is how we let server.js know how to grab this module!
 exports.start = start;
